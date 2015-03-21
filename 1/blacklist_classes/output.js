987d7c65b67e1d01dd7c0db8d4af94c9fb52dd74
nakajmg <nakajima.jmg@gmail.com>
2015-03-21 15:30:27 +0900

    add sample

---
+++ 1/blacklist_classes/output.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+var App = function App() {
+  _classCallCheck(this, App);
+
+  console.log("class syntax");
+};
+
+var func = function () {
+  console.log("arrow function");
+};
